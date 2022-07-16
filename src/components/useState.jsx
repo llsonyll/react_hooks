74bc2d2534d4d4bb5b5af2a752cba4281b50561c
llsonyll <sony_s07@hotmail.es>
2022-07-16 09:05:32 -0500

    initial commit

---
+++ src/components/useState.jsx
@@ -0,0 +1,9 @@
+const useState = () => {
+  return (
+    <div className="useStateHook">
+      <h4> Use State Hook </h4>
+    </div>
+  );
+};
+
+export default useState;
---
+++ src/reactHook.jsx
@@ -0,0 +1,13 @@
+import UseStateHook from "./components/useState";
+
+import "./reactHook.css";
+
+const ReactHooks = () => {
+  return (
+    <div className="reactHook">
+      <UseStateHook />
+    </div>
+  );
+};
+
+export default ReactHooks;
